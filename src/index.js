cb3514f68a628518920b5d47e3be18154e2d7e5e
guilhermeicarofr <guilhermeicarofr@gmail.com>
2023-03-03 01:23:21 -0300

    feat: created counter to validate when user really wants to present gesture to play

--- src/index.js
+++ src/index.js
@@ -5,10 +5,24 @@ import getVideoFrame from './modules/videoFrame.js';
 
 const video = await runCamStream();
 
-setInterval(async () => {
+let playGesture = { count: 0, gesture: ''};
+
+const interval = setInterval(async () => {
   const frame = getVideoFrame(video);
   const hands = await detectHands(frame);
   const gesture = await detectGesture(hands);
 
-  console.log(`hands:${(hands.length)?'up':'down'}`, gesture);
+  console.log(`hands:${(hands.length)?'up':'down'}`, playGesture);
+
+  if(gesture.name && gesture.name === playGesture.gesture) {
+    playGesture.count++;
+  } else {
+    playGesture.count = 0;
+    playGesture.gesture = gesture.name;
+  }
+
+  if(playGesture.count >= 15) {
+    console.info(playGesture.gesture);
+    playGesture = { count: 0, gesture: ''};
+  }
 }, 100);
